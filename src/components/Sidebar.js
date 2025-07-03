56af79c32f8b9c071cb8550a1a78a719985b5d44
HoangViet1411 <vietdai47@gmail.com>
2025-07-03 22:00:15 +0700

    update fix some bug

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import logo from '../assets/logo.png'; // Đảm bảo bạn có file logo tại src/assets/logo.png
+import logo from '../assets/logo.png';
 
 const Sidebar = () => {
   const navigate = useNavigate();
